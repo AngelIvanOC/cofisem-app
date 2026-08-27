a380539abe6a46bfeef3c689f9715628073eb0b5
AngelIvan999 <angelivanortegachavarria@gmail.com>
2026-08-27 12:34:14 -0600

    Aceptando todos los audioss

--- src/features/cabinero/components/BotonAudioSiniestro.jsx
+++ src/features/cabinero/components/BotonAudioSiniestro.jsx
@@ -134,7 +134,7 @@ export default function BotonAudioSiniestro({
                   <>
                     <Mic className="w-5 h-5 text-gray-400" />
                     <span className="text-xs text-gray-500 font-medium">
-                      Seleccionar o grabar audi
+                      Seleccionar o grabar audio
                     </span>
                   </>
                 )}
