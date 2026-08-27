28e49330f5785547026b0835ee32fac8a1545b49
AngelIvan999 <angelivanortegachavarria@gmail.com>
2026-08-27 12:24:24 -0600

    Aceptando todos los audioss

--- src/features/cabinero/components/BotonAudioSiniestro.jsx
+++ src/features/cabinero/components/BotonAudioSiniestro.jsx
@@ -140,7 +140,7 @@ export default function BotonAudioSiniestro({
                 )}
                 <input
                   type="file"
-                  accept="audio/*"
+                  accept="audio/*,.acc"
                   hidden
                   disabled={subiendo}
                   onChange={handleFile}
